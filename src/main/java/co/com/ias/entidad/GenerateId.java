e22cfd2e5a3b247e0d7cf21d2ab6cd2bb0033e8e
LINAH HENAO <you@example.com>
2018-11-04 13:52:13 -0500

    .

---
+++ src/main/java/co/com/ias/entidad/GenerateId.java
@@ -0,0 +1,17 @@
+package co.com.ias.entidad;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.id.IdentifierGenerator;
+
+public class GenerateId  implements IdentifierGenerator {
+
+	@Override
+	public Serializable generate(SharedSessionContractImplementor session, Object object) throws HibernateException {
+			return "AV"+ UUID.randomUUID().toString();
+	}
+
+}
